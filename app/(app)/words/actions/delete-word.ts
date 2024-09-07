480c30d74aaf0788163ae954f562bd00e85be9a5
Konstantin Rybakov <i@krybakov.com>
2024-09-07 11:54:19 +0300

    wlth-83: fix word deletion

--- app/(app)/words/actions/delete-word.ts
+++ app/(app)/words/actions/delete-word.ts
@@ -7,8 +7,12 @@ import 'server-only'
 export const deleteWord = async (id: number) => {
   'use server'
 
-  const result = await prisma.word.delete({
-    where: { id },
+  const result = await prisma.wordData.deleteMany({
+    where: {
+      word: {
+        id,
+      },
+    },
   })
 
   return result
